7cb4b937c52ab77a0c8882b9655cf9eb0afb58bd
Jeet <slele@redhat.com>
2024-03-18 15:42:17 -0400

    Change auth functioning

--- app/src/components/auth.py
+++ app/src/components/auth.py
@@ -1,33 +1,40 @@
 import streamlit as st
 from msal_streamlit_authentication import msal_authentication
 
-def creds_entered():
-    value = msal_authentication(
-    auth={
-        "clientId": "95d75130-08e9-4d56-935f-1cc063a81177",
-        "authority": "https://login.microsoftonline.com/64dc69e4-d083-49fc-9569-ebece1dd1408",
-        "redirectUri": "https://streamlit-auth-package-streamlit.apps.cluster-srkhk.dynamic.redhatworkshops.io",
-        "postLogoutRedirectUri": "/"
-    },
-    cache={
-        "cacheLocation": "sessionStorage",
-        "storeAuthStateInCookie": False
-    },
-    )
-    if value is not None:
-        st.session_state["authenticated"] = True    
-        st.write("Received", value)
-    else:
-        st.error("Invalid Username/Pass :face_with_raised_eyebrow: :man-shrugging: :yawning_face:")
+# def creds_entered():
+    
+    
 
 
 def authenticate_user():
+    value = None
     if "authenticated" not in st.session_state:
-        st.button("Login with Azure", on_click=creds_entered)
+        # st.button("Login with Azure", on_click=creds_entered)
+        value = msal_authentication(
+        auth={
+            "clientId": "95d75130-08e9-4d56-935f-1cc063a81177",
+            "authority": "https://login.microsoftonline.com/64dc69e4-d083-49fc-9569-ebece1dd1408",
+            "redirectUri": "https://streamlit-auth-package-streamlit.apps.cluster-srkhk.dynamic.redhatworkshops.io",
+            "postLogoutRedirectUri": "/"
+        },
+        cache={
+            "cacheLocation": "sessionStorage",
+            "storeAuthStateInCookie": False
+        },
+        )
+    if value is not None:
+        st.session_state["authenticated"] = True    
+        st.write("Received", value)
+        return True
     else:
-        if st.session_state["authenticated"]:
-            return True
-        else:
-            st.button("Login with Azure", on_click=creds_entered)
-            return False
+        st.error("Invalid Username/Pass :face_with_raised_eyebrow: :man-shrugging: :yawning_face:")
+        return False
+    
+    
+    # else:
+    #     if st.session_state["authenticated"]:
+    #         return True
+    #     else:
+    #         st.button("Login with Azure", on_click=creds_entered)
+    #         return False
     
