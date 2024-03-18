a0781767ddaff2d0b03d5c068d6cccf6f375d2fd
Jeet <slele@redhat.com>
2024-03-18 17:05:31 -0400

    latest auth code

--- app/src/components/auth.py
+++ app/src/components/auth.py
@@ -8,15 +8,16 @@ from loguru import logger
 
 
 def authenticate_user():
-    if "authenticated" not in st.session_state or st.session_state["authenticated"] == False:
+    if "authenticated" not in st.session_state:
         # st.button("Login with Azure", on_click=creds_entered)
-        logger.debug("Start auth")
+        logger.debug(f"Start auth:")
         st.subheader("Please login using Single Sign On")
         value = msal_authentication(
         auth={
             "clientId": "95d75130-08e9-4d56-935f-1cc063a81177",
             "authority": "https://login.microsoftonline.com/64dc69e4-d083-49fc-9569-ebece1dd1408",
-            "redirectUri": "https://streamlit-auth-package-streamlit.apps.cluster-srkhk.dynamic.redhatworkshops.io",
+            # "redirectUri": "https://streamlit-auth-package-streamlit.apps.cluster-srkhk.dynamic.redhatworkshops.io",
+            "redirectUri": "http://localhost:8501/Home",
             "postLogoutRedirectUri": "/"
         },
         cache={
@@ -25,17 +26,20 @@ def authenticate_user():
         },
         )
         if value is not None:
-            logger.debug("Value is not set")
-            st.session_state["authenticated"] = True    
+            st.session_state["authenticated"] = True
+            logger.debug(f"Value is going to be set: {st.session_state['authenticated']}")    
             st.write("Received", value)
             return True  
-        else:
-            logger.debug("Value is set")
-            st.session_state["authenticated"] = False
-            st.error("Unable to login")
+        # else:
+        #     st.session_state["authenticated"] = False
+        #     logger.debug(f"value is None: {st.session_state['authenticated']}")
+        #     st.error("Unable to login")
     else:
-        logger.debug("Returning False")     
-        return False
+        logger.debug(f"Returning False: {st.session_state['authenticated']}")
+        if st.session_state['authenticated']:
+            return True
+        else:     
+            return False
     
     
     # else:
