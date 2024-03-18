1d69abca99825b9712ff7387ca38a8ce97db5569
Jeet <slele@redhat.com>
2024-03-18 16:15:49 -0400

    rework auth logic

--- app/src/components/auth.py
+++ app/src/components/auth.py
@@ -7,8 +7,9 @@ from msal_streamlit_authentication import msal_authentication
 
 
 def authenticate_user():
-    if "authenticated" not in st.session_state:
+    if "authenticated" not in st.session_state or st.session_state["authenticated"] == False:
         # st.button("Login with Azure", on_click=creds_entered)
+        st.subheader("Please login using Single Sign On")
         value = msal_authentication(
         auth={
             "clientId": "95d75130-08e9-4d56-935f-1cc063a81177",
@@ -21,12 +22,14 @@ def authenticate_user():
             "storeAuthStateInCookie": False
         },
         )
-    if value is not None:
-        st.session_state["authenticated"] = True    
-        st.write("Received", value)
-        return True
-    else:
-        st.error("Invalid Username/Pass :face_with_raised_eyebrow: :man-shrugging: :yawning_face:")
+        if value is not None:
+            st.session_state["authenticated"] = True    
+            st.write("Received", value)
+            return True  
+        else:
+            st.session_state["authenticated"] = False
+            st.error("Unable to login")
+    else:     
         return False
     
     
